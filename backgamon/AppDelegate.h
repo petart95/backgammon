af8f462993326de9b909efdf95459398e4677dd8
Petar Trifunovic <ptrifunovic@maxeler.com>
2017-01-20 17:10:44 +0100

    Initial Commit

---
+++ backgamon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  backgamon
+//
+//  Created by maxeler on 1/20/17.
+//  Copyright © 2017 maxeler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ backgamon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  backgamon
+//
+//  Created by maxeler on 1/20/17.
+//  Copyright © 2017 maxeler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
